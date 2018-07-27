ccc735087ef05133b15648c8e55cf34a54f77cc2
Toxiedo <eng.islam.hamdy1992@gmail.com>
2018-07-28 01:31:04 +0200

    more problems solving

---
+++ C++ Tutorial Hackerrank/3-Basic Data Types/Solution.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+   /* int x;
+     long x1;
+        long long x2;
+        char x3;
+        float x4;
+        double x5;
+    cin>>x;
+    cout<<x<<endl;
+    cin>>x1;
+    cout<<x1<<endl;
+    cin>>x2;
+    cout<<x2<<endl;
+    cin>>x3;
+    cout<<x3<<endl;
+    cin>>x4;
+    cout<<x4<<endl;
+    cin>>x5;
+    cout<<x5<<endl;
+     */
+        string s;
+    for(int i=0; i < 6; i++) {
+        cin >> s;
+        cout << s << endl;
+    }
+    return 0;
+}
